7fe10d5bb96803ef1ed0a1f6574a68e072006efa
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-02 23:26:41 +0300

    Add changes to html and js files

--- js/script.js
+++ js/script.js
@@ -5,48 +5,48 @@ function submitQuiz() {
   function correctAnswer (correctStringNo, qNumber) {
     console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
     return ("Correct answer: &nbsp;<strong>" +
-    (document.getElementById(correctStringNo).innerHTML) + "</strong>");
-  }
+      (document.getElementById(correctStringNo).innerHTML) + "</strong>");
+    }
 
   // print correct answers only if wrong (calls correctAnswer function)
-  function correctAnswer() {
-    if (answerScore('q1') === 0) {
-      document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-    }
-    if (answerScore('q2') === 0) {
-      document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-    }
-    if (answerScore('q3') === 0) {
-      document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
-    }
-    if (answerScore('q4') === 0) {
-      document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
-    }
-    if (answerScore('q5') === 0) {
-      document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctString5', 5);
-    }
-    if (answerScore('q6') === 0) {
-      document.getElementById('correctAnswer6').innerHTML = correctAnswer('correctString6', 6);
-    }
-    if (answerScore('q7') === 0) {
-      document.getElementById('correctAnswer7').innerHTML = correctAnswer('correctString7', 7);
-    }
-    if (answerScore('q8') === 0) {
-      document.getElementById('correctAnswer8').innerHTML = correctAnswer('correctString8', 8);
-    }
-    if (answerScore('q9') === 0) {
-      document.getElementById('correctAnswer9').innerHTML = correctAnswer('correctString9', 9);
-    }
-    if (answerScore('q10') === 0) {
-      document.getElementById('correctAnswer10').innerHTML = correctAnswer('correctString10', 10);
-    }
+  if (answerScore('q1') === 0) {
+    document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
   }
+  if (answerScore('q2') === 0) {
+    document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
+  }
+  if (answerScore('q3') === 0) {
+    document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
+  }
+  if (answerScore('q4') === 0) {
+    document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
+  }
+  if (answerScore('q5') === 0) {
+    document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctString5', 5);
+  }
+  if (answerScore('q6') === 0) {
+    document.getElementById('correctAnswer6').innerHTML = correctAnswer('correctString6', 6);
+  }
+  if (answerScore('q7') === 0) {
+    document.getElementById('correctAnswer7').innerHTML = correctAnswer('correctString7', 7);
+  }
+  if (answerScore('q8') === 0) {
+    document.getElementById('correctAnswer8').innerHTML = correctAnswer('correctString8', 8);
+  }
+  if (answerScore('q9') === 0) {
+    document.getElementById('correctAnswer9').innerHTML = correctAnswer('correctString9', 9);
+  }
+  if (answerScore('q10') === 0) {
+    document.getElementById('correctAnswer10').innerHTML = correctAnswer('correctString10', 10);
+  }
+
   // get each answer score
   function answerScore (qName) {
     var radiosNo = document.getElementsByName(qName);
+
     for (var i = 0, length = radiosNo.length; i < length; i++) {
-      if (radiosNo[i].checked) {
-        // do something with radiosNo
+         if (radiosNo[i].checked) {
+    // do something with radiosNo
         var answerValue = Number(radiosNo[i].value);
       }
     }
